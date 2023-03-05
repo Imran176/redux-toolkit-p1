63aec9f2a9b4b16b53bedf475614777d550dad3a
Imran176 <imran.haider28598@gmail.com>
2023-03-05 18:18:53 +0500

    react+redux_toolkit+redux_thunk+redux_saga

--- src/features/posts/postSagas.js
+++ src/features/posts/postSagas.js
@@ -1,8 +1,6 @@
 import { all, call, put, takeLatest } from "redux-saga/effects";
-import axios from "axios";
 
 import { fetchAsyncPosts, fetchAsyncPhotos } from "./postSlice";
-// import { fetchAsyncPosts, fetchAsyncPhotos } from "./postSlice";
 import {
   fetchPostsStart,
   fetchPostsSuccess,
@@ -13,21 +11,12 @@ import {
 } from "./postSlice";
 
 export function* fetchAllPostsAsync() {
-  console.log("Fetching Posts using Saga");
   try {
-    console.log("Saga try block");
-
     const result = yield call(fetchAsyncPosts);
-    console.log("Api result: ", result);
-    // yield put(actions.getUsersSuccess(result.data.data));
-    // const response = yield call(fetchAsyncPosts());
     const { data } = result;
-    yield console.log("Api response data: ", data);
-
     yield put(fetchPostsSuccess(data));
   } catch (error) {
     yield put(fetchPostsFailure(error.message));
-    console.log("saga failure error: ", error.message);
   }
 }
 
@@ -35,25 +24,17 @@ export function* fetchAllPostsStart() {
   try {
     yield takeLatest(fetchPostsStart.type, fetchAllPostsAsync);
   } catch (error) {
-    console.log("Error in Saga", error);
+    console.log("Error in Posts Saga", error);
   }
 }
 
 export function* fetchAllPhotosAsync() {
-  console.log("Fetching Photos using Saga");
   try {
-    console.log("Photos Saga try block");
-
     const response = yield call(fetchAsyncPhotos);
-    console.log("Photos Api result: ", response);
-    
     const { data } = response;
-    yield console.log("Photos Api response data: ", data);
-
     yield put(fetchPhotosSuccess(data));
   } catch (error) {
     yield put(fetchPhotosFailure(error.message));
-    console.log("Photos saga failure error: ", error.message);
   }
 }
 
